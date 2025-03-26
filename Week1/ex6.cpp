26a441d55f1aafd6fcafa12f29098327d04022ca
ShuVChi <trongnguyenx1306@gmail.com>
2025-03-26 20:56:00 +0700

    exercise week1

--- Week1/ex6.cpp
+++ Week1/ex6.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 using namespace std;
-const int N = 5; // N-Queens size
-int board[N][N] = { 0 }; // 8x8 Chessboard
-int solutions = 0; // Count valid solutions
+#define N 10 // N-Queens size
+int board[N][N] = { 0 };
+int solutions = 0; // dem so cach giai
 
-// Function to check if a queen can be placed at board[row][col]
-bool isSafe(int row, int col)
-{
+// check if a queen can be placed at board[row][col]
+bool isSafe(int row, int col){
 	for (int i = 0; i < row; i++)
 		{
 		if (board[i][col] == 1)
-			return false; // Check column
+			return false; // Check col
 		
 			if (col - (row - i) >= 0 && board[i][col - (row - i)] == 1)
-			return false; // Check left diagonal
+			return false; // Check cheo trai
 	
 			if (col + (row - i) < N && board[i][col + (row - i)] == 1)
-			 return false; // Check right diagonal
+			 return false; // Check cheo phai
 		}
 	return true;
 }
-
-void printSolution(int board[N][N]) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-			cout << board[i][j] << " ";
-		cout << endl;
-	}
-}
-
-// Backtracking function to place queens
-void solveNQueens(int row)
-{
+//backtracking place queens
+void solveNQueens(int row){
 	if (row == N) 
-		{ // All queens placed successfully
+		{ // tat ca Queens da duoc dat-> tang solution
 		solutions++;
-		printSolution(board);
-		cout << endl;
 		return;
 		}
 	for (int col = 0; col < N; col++) 
@@ -52,7 +39,7 @@ void solveNQueens(int row)
 
 int main(){
 	solveNQueens(0);
-	cout <<"Number of solutions: "<<solutions << endl;
+	cout <<"Number of solutions ("<<N<<"x"<<N<< "): " << solutions << endl;
 	return 0;
 }
 
